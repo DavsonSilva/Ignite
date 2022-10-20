6ec823ff20ed06c6ba8e1cfd809a9e64d62060ba
Davson silva <davson1@hotmail.com>
2022-10-20 22:25:24 +0100

    project finished

---
+++ lab-ds/.storybook/main.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y'
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
+    }
+
+    return config
+  }
+}
+// module.exports = {
+//   stories: ['../src/**/*.stories.tsx'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/addon-interactions',
+//     '@storybook/addon-a11y',
+//   ],
+//   typescript: {
+//     reactDocgen: 'react-docgen-typescript',
+//     reactDocgenTypescriptOptions: {
+//       compilerOptions: {
+//         allowSyntheticDefaultImports: false,
+//         esModuleInterop: false,
+//       },
+//     },
+  
+//   }
+// };
